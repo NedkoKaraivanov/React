cd7dd96e13b1c8e616202bb87d2a2ffa0b05d760
Nedko Karaivanov <nedko2016@gmail.com>
2023-06-17 09:45:23 +0300

    Sample Example of questions section using useState

---
+++ Exercise-React/04-fundamental-projects/04-accordion/starter/src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: "Is Java hard to learn??",
+    info: "Learning Java is only hard when you have no technical background or you don't take the proper steps to learn the language. Java syntax is a statically typed machine language that has broad features and frameworks and can be integrated into several platforms. This can make it difficult to grasp as a beginner.",
+  },
+  {
+    id: 2,
+    title: "What is faster than React?",
+    info: "Svelte is faster than React as, at the build time the components are compiled so that when the user interacts with the app, the extra work of seeing which part of the DOM has to be updated is minimized.",
+  },
+  {
+    id: 3,
+    title: "Is Java the future of programming?",
+    info: "Java has a bright future. It is widely used in many industries, including financial services, retail, and healthcare. In addition, newer technologies like artificial intelligence and the internet of things are being built using Java.",
+  },
+  {
+    id: 4,
+    title: "Is Java still worth learning in 2023?",
+    info: "The answer is a resounding yes! Even though there are newer languages out there, Java has remained popular due to its versatility and robustness. It can be used for everything from developing Android apps to building enterprise-level applications.",
+  },
+  {
+    id: 5,
+    title: "What's the difference between Java and JavaScript?",
+    info: "Java and JavaScript are similar in the same way car and carpet are. :)",
+  },
+  {
+    id: 6,
+    title: "How long does take to learn Java??",
+    info: "The average estimate for a beginner to learn Java is about nine months. For someone who already knows how to code, learning Java could take as little as a month. Experts say that it might take up to three months, depending on how much you practice.",
+  },
+];
+export default questions;
